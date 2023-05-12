474329a062478644be8133b69bedf1b4a82ba679
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-12 23:27:50 +0200

    Avatar component used twice in Profile component

--- ex-2 props/src/App.js
+++ ex-2 props/src/App.js
@@ -14,12 +14,22 @@ function Avatar({ person, size }) {
 
 export default function Profile() {
   return (
-    <Avatar
-      size={91}
-      person={{
-        name: "Gregorio Y. Zara",
-        imageId: "7vQD0fP"
-      }}
-    />
+    <>
+      <Avatar
+        size={40}
+        person={{
+          name: "Gregorio Y. Zara",
+          imageId: "7vQD0fP"
+        }}
+      />
+
+      <Avatar
+        size={91}
+        person={{
+          name: "Gregorio Y. Zara",
+          imageId: "7vQD0fP"
+        }}
+      />
+    </>
   );
 }
